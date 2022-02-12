67d1ccf2bf0ff5a84b44d69d8a65ba57f3f6c37c
Andreas Jakobsson <Andreas8311@hotmail.com>
2022-02-12 20:07:12 +0100

    Version 0.02

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pandas as pd
 
 st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
+df = pd.DataFrame({
     'first column': [1, 2, 3, 4, 5],
     'second column': [10, 20, 30, 40, 50]
-}))
+})
 
 
 add_selectbox = st.sidebar.selectbox(
@@ -14,10 +14,8 @@ add_selectbox = st.sidebar.selectbox(
 )
 
 with st.expander("Click here if you want to  know more", expanded=False):
-    st.write("""
-             And here you have some useless stuff.
-         """)
-    st.write("Here wz write some useless stiff")
+    st.line_chart(data=df, width=10, height=10, use_container_width=True)
+    st.write("Here we write some useless stiff")
 with st.expander("second stuff", expanded=False):
     st.write("""
              and even more details
