3271bc2d692f7e8f3e56edb00d6999e61f31392d
Andreas Jakobsson <Andreas8311@hotmail.com>
2022-02-12 19:51:11 +0100

    Version 0.02

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,13 @@ st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4, 5],
     'second column': [10, 20, 30, 40, 50]
 }))
+
+
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
+
+
+
+add_selectbox.write('Hello again')
