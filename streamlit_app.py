e2ae1a0fab789d665d2e67c9037fc2ca499b2171
Andreas Jakobsson <Andreas8311@hotmail.com>
2022-02-13 07:59:38 +0100

    Version 0.04 trying bar chart

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,26 +2,15 @@ import streamlit as st
 import pandas as pd
 
 
-df = pd.DataFrame({
-    'first column': [1, 2, 3, 4, 5],
-    'second column': [10, 20, 30, 40, 50]
-})
+
 
 # Introduction text
 
-st.subheader('Andreas Jakobsson Programming and Data Science')
+st.title('Andreas Jakobsson Programming and Data Science')
 st.write("""
-             On this page you find a selection of baseline projects and technical skills.
+             On this page you find a selection of my baseline projects and technical skills.
              This page should be complemented with my official CV or with my Linkedin profile
          """)
-
-st.header('My header')
-st.subheader('My sub')
-
-
-
-
-
 
 
 add_selectbox = st.sidebar.selectbox(
@@ -29,8 +18,14 @@ add_selectbox = st.sidebar.selectbox(
     ("Email", "Home phone", "Mobile phone")
 )
 
-with st.expander("Click here if you want to  know more", expanded=False):
-    st.line_chart(data=df)
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4, 5],
+    'second column': [10, 20, 30, 40, 50]
+})
+
+
+with st.expander("Data Structures and Algorithms", expanded=False):
+    st.bar_chart(data=df)
     st.write("Here we write some useless stiff")
 with st.expander("second stuff", expanded=False):
     st.write("""
