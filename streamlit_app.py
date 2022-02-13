781d31be2f689b96897f759f9d9d455704239e45
Andreas Jakobsson <Andreas8311@hotmail.com>
2022-02-13 07:34:12 +0100

    Version 0.02

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,23 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(
-     page_title="My Interactive CV and AI portfolio",
-     page_icon="🧊",
-     layout="wide",
-     initial_sidebar_state="expanded",
-     menu_items={
-         'Get Help': 'https://www.extremelycoolapp.com/help',
-         'Report a bug': "https://www.extremelycoolapp.com/bug",
-         'About': "# This is a header. This is an *extremely* cool app!"
-     }
- )
-
-
 
 st.write("Here's our first attempt at using data to create a table:")
 df = pd.DataFrame({
     'first column': [1, 2, 3, 4, 5],
     'second column': [10, 20, 30, 40, 50]
 })
+
+# Random text
+
+st.title('My title')
+st.header('My header')
+st.subheader('My sub')
+
+st.write('Most objects') # df, err, func, keras!
+st.write(['st', 'is <', 3]) # see *
+
+
 
 
 add_selectbox = st.sidebar.selectbox(
