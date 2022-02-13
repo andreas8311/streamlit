74711f6c5878c11a0b807ece09c413af34ecbefd
Andreas Jakobsson <Andreas8311@hotmail.com>
2022-02-13 08:34:55 +0100

    Version 0.04 trying bar chart

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,24 @@
 import streamlit as st
 import pandas as pd
-#import plotly.graph_objects as go
-import plotly.express as px
+import plotly.figure_factory as ff
+import numpy as np
+
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
+
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+         hist_data, group_labels, bin_size=[.1, .25, .5])
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
 
 
 
@@ -24,7 +41,7 @@ df = pd.DataFrame({
     'columns': ['Graph Theory', 'Path Finding', 'test3', 'test4', 'test5']
 })
 
-
+"""
 with st.expander("Data Structures and Algorithms", expanded=False):
 
     fig = px.bar(df, x='columns', y='first column')
@@ -32,6 +49,7 @@ with st.expander("Data Structures and Algorithms", expanded=False):
     # Plot!
     st.plotly_chart(fig, use_container_width=True)
     st.write("Here we write some useless stiff")
+"""
 with st.expander("second stuff", expanded=False):
     st.write("""
              and even more details
